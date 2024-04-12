91583fdabe79615350fed2d0b9e2457511b8d447
krkarma777 <krkarma777@gmail.com>
2024-04-12 15:25:29 +0900

    Initial commit

---
+++ src/main/java/com/jasoseobot/JasoseoBotApplication.java
@@ -0,0 +1,13 @@
+package com.jasoseobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasoseoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasoseoBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jasoseobot/JasoseoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasoseobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasoseoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
